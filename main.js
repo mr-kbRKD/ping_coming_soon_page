e540135fbf44c05ea665f5f11d261c80e07a64cb
KULBHUSHAN <krishnathakur0007895@gmail.com>
2023-06-14 17:33:49 +0530

    Updated validateEmail function but still it's not working

--- main.js
+++ main.js
@@ -6,19 +6,24 @@ function checkEmail(e){
     const email = e.target.querySelector('[type="email"]').value;
     validateEmail(email);
         if(!validateEmail){
-            console.log("Invalid email");
+            // console.log("Invalid email");
+            form.classList.add('error');
         }
         else{
-            console.log("Valid email");
+            // console.log("Valid email");
+            form.classList.remove('error');
         }
         
         console.log(email);
         console.log(e);
     }
 
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  }
+    const validateEmail = (email) => {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
 
 form.addEventListener('submit', checkEmail);
